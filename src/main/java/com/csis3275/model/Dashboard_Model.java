da234ccf534e1ab16f701c70a22fff19093ef1cb
Jack Braun <braunjack2003@gmail.com>
2023-11-13 17:25:22 -0800

    Feat: added string about, getters, setters, and constructor

--- src/main/java/com/csis3275/model/Dashboard_Model.java
+++ src/main/java/com/csis3275/model/Dashboard_Model.java
@@ -2,4 +2,18 @@ package com.csis3275.model;
 
 public class Dashboard_Model {
 	String about;
+
+	public String getAbout() {
+		return about;
+	}
+
+	public void setAbout(String about) {
+		this.about = about;
+	}
+
+	public Dashboard_Model(String about) {
+		super();
+		this.about = about;
+	}
+	
 }
