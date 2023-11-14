8c59490794fd2cba4091af38240fa1b2892eb3c2
Kithmal Wimalaratne <wimalaratnek@student.douglascollege.ca>
2023-11-13 20:19:49 -0800

    feat: dashboard controller changes and added my view

--- src/main/java/com/csis3275/controller/DashboardController.java
+++ src/main/java/com/csis3275/controller/DashboardController.java
@@ -11,6 +11,8 @@ public class DashboardController {
 	
 	Jack_FREELANCECONNECT jack = new Jack_FREELANCECONNECT("My name is Jack i was born in canada. I like playing video games, skiing and taking my two dogs on walks.","Jack.jpg");
 	
+	Kithmal_FREELANCECONNECT kithmal = new Kithmal_FREELANCECONNECT("I'm Kithmal. A student studying CSIS-Cybersecurity at Douglas College. I am new to Canada and it has been a wonderful experience here so far.", "Kithmal.jpg");
+	
 	DashboardModel dash = new DashboardModel("Freelance connect is an application meant to make communication between freelancers and employers easier");
 	
 	@GetMapping("/")
@@ -25,5 +27,11 @@ public class DashboardController {
 	{
 		model.addAttribute("jack",jack);
 		return "jack";
+	}
+	
+	@GetMapping("/kithmal")
+	public String kithmal(Model model) {
+		model.addAttribute("kithmal", kithmal);
+		return "kithmal";
 	}
 }
