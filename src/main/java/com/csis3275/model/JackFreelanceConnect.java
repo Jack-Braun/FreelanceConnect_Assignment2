4da39e42d3e75690be2ba5951a2276b145f9019b
Jack Braun <braunjack2003@gmail.com>
2023-11-13 18:02:15 -0800

    fix: fixed broken constructor

--- src/main/java/com/csis3275/model/JackFreelanceConnect.java
+++ src/main/java/com/csis3275/model/JackFreelanceConnect.java
@@ -1,5 +1,25 @@
 package com.csis3275.model;
 
 public class JackFreelanceConnect {
-
+	String bio;
+	String img;
+	
+	public String getBio() {
+		return bio;
+	}
+	public void setBio(String bio) {
+		this.bio = bio;
+	}
+	public String getImg() {
+		return img;
+	}
+	public void setImg(String img) {
+		this.img = img;
+	}
+	public JackFreelanceConnect(String bio, String img) {
+		super();
+		this.bio = bio;
+		this.img = img;
+	}
+	
 }
