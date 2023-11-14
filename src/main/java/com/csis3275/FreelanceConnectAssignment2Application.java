cfd42cb48b7686a222c5d6785f929214cdd2cc08
Jack Braun <braunjack2003@gmail.com>
2023-11-13 17:18:01 -0800

    Build: Initial Commit

---
+++ src/main/java/com/csis3275/FreelanceConnectAssignment2Application.java
@@ -0,0 +1,13 @@
+package com.csis3275;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreelanceConnectAssignment2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreelanceConnectAssignment2Application.class, args);
+	}
+
+}
